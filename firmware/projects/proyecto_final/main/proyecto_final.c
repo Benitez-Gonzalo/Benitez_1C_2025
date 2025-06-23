c865a7463a5e879bc67201955e6e981a6e516dd3
Gonzalo Benítez <gonzalo.benitez@ingenieria.uner.edu.ar>
2025-06-23 06:50:10 -0300

    Se actualizó la documentación del proyecto final y se eliminaron comentarios innecesarios.

--- firmware/projects/proyecto_final/main/proyecto_final.c
+++ firmware/projects/proyecto_final/main/proyecto_final.c
@@ -1,5 +1,5 @@
 
-/*! @mainpage P.Electronics System
+/*! @mainpage Final Project
  *
  * @section genDesc General Description
  *
@@ -8,18 +8,22 @@
  * connected between R1 and GND. The voltage is read using the analog_io_mcu driver, and the resistance is
  * calculated and displayed via the serial port. Then, the value is send through Telegram to a bot periodically.
  * This value is used to calculate water conductivity (using the water as R2) and its hardness. If this last value is
- * too high, a filter is deployed into the water sample.
+ * too high, a filter is deployed into the water sample. Momentarilly, the filter is represented as an LED due to the ESP
+ * lack of power.
  *
  * <a href="https://drive.google.com/...">Operation Example</a>
  *
  * @section hardConn Hardware Connection
  *
- * |    Peripheral          |   ESP32-C6   	|
- * |:----------------------:|:-----------------|
- * | 	R1 (10 kOhm) (+)      | 	3.3V			|
- * | 	R1 (10 kOhm) (-)      | 	GPIO0 (CH0)		|
- * | 	R2 (Unknown) (+)      | 	GPIO0 (CH0)		|
- * | 	R2 (Unknown) (-)      | 	GND				|
+ * |    Component / Node                      |   ESP32-C6         |
+ * |:----------------------------------------:|:-------------------:|
+ * | GPIO9 (PWM output)                       | → Ceramic Capacitor (104) |
+ * | Capacitor (104) output                   | → R1 (10 kΩ)         |
+ * | R1 output                                | → Potentiometer (25 kΩ) |
+ * | Potentiometer output                     | → Positive Electrode (Anode) |
+ * | Junction between R1 and Potentiometer    | → GPIO0 (ADC CH0)    |
+ * | Negative Electrode (Cathode)             | → GND                |
+ * | GPIO11                                   | → LED → R (1 kΩ) → GND |
  *
  * @section changelog Changelog
  *
@@ -28,6 +32,9 @@
  * | 12/09/2023 | Document creation		                         |
  * | 12/05/2025 | Updated for ESP32-C6 and ESP-IDF v5.4.0        |
  * | 12/05/2025 | Using analog_io_mcu driver for ADC readings    |
+ * | 12/06/2025 | Created Telegram Bot with its driver           |
+ * | 18/06/2025 | The flash partition size was increased         |
+ * | 21/06/2025 | PWM was used to impedance measurements         |
  *
  * @author Gonzalo Benitez (gonzalo.benitez@ingenieria.uner.edu.ar)
  *
@@ -41,7 +48,7 @@
 #include "analog_io_mcu.h"
 #include "uart_mcu.h"
 #include "telegram_bot_mcu.h"
-#include "servo_sg90.h"
+#include "led.h"
 #include "pwm_mcu.h"
 /*==================[macros and definitions]=================================*/
 #define R1  10000.0 // Ohm (resistencia conocida)
@@ -51,7 +58,7 @@
 #define WIFI_PASS "gonza235"
 #define BOT_TOKEN "7666661023:AAGC_aNd2ElAc5ieqiJxhCb8IVP74LCZG1o"
 #define CHAT_ID "7725635002" 
-#define UMBRAL_KOHM 5 //Umbral resistivo en kohm
+#define UMBRAL 10 //Umbral de dureza en mg/L
 #define CONVERSION_FACTOR 0.145
 /*==================[internal data definition]===============================*/
 TaskHandle_t measuringResistanceTask = NULL;
@@ -64,22 +71,9 @@ float r_water = 0;
 float conductivity = 0;
 float water_hardness = 0;
 char buffer[100];
-char message[128];
 extern const uint8_t _binary_telegram_cert_pem_start[];
+static uint8_t led_state = 255; // Inicialmente inválido
 /*==================[internal functions declaration]=========================*/
-
-void showVoltage(uint16_t value){
-    UartSendString(UART_PC, "El voltaje medido es: ");
-	UartSendString(UART_PC, (char*)UartItoa(value, 10));
-	UartSendString(UART_PC, "mV\n");
-}
-
-void showResistance(uint32_t value){
-    UartSendString(UART_PC, "La resistencia medida es: ");
-	UartSendString(UART_PC, (char*)UartItoa(value, 10));
-	UartSendString(UART_PC,"ohm");
-    UartSendString(UART_PC,"\r\n");
-}
 
 void measuringResistanceCallBack (void){
     vTaskNotifyGiveFromISR(measuringResistanceTask,pdFALSE);
@@ -90,43 +84,16 @@ void telegramMessageCallBack (void){
 }
 
 /**
- * @brief Tarea encargada de la medición de la resistencia con DC
-
-static void measuringResistanceFunction(void *pvParameter){
-    while(true){
-        ulTaskNotifyTake(pdTRUE, portMAX_DELAY);
-        AnalogInputReadSingle(CH0,&voltage);
-        showVoltage(voltage);
-
-        // Calcular R2 a partir del divisor de voltaje
-        float Vout = voltage / 1000.0; // Convertir a voltios
-        float Vcc = 3.3; // Voltaje de suministro
-        if (Vout > 0 && Vout < Vcc) {
-            resistance = (Vout * R1) / (Vcc - Vout);
-            showResistance(resistance);
-            if(resistance > UMBRAL_KOHM * 1000){ServoMove(SERVO_0,90);}
-            else{ServoMove(SERVO_0,0);}
-        } else {
-            UartSendString(UART_PC, "Error, voltaje fuera de rango para cálculo");
-            UartSendString(UART_PC, (char*)UartItoa(Vout, 10));
-            UartSendString(UART_PC, "V\n");
-        }  
-    }
-}
-*/
-
-/**
  * @brief Tarea encargada de enviar el mensaje vía Telegram
 */
 static void telegramMessageFunction(void *pvParameter) {
     while (true) {
         ulTaskNotifyTake(pdTRUE, portMAX_DELAY);
 
-        //snprintf(message, sizeof(message), "Resistencia medida: %lu ohm", resistance);
+        //snprintf(buffer, sizeof(buffer), "Resistencia medida = %lu ohm", r_water);
 
-        snprintf(buffer, sizeof(buffer),"Cwater = %.0f µS/cm\n", r_water);
+        snprintf(buffer, sizeof(buffer),"Dureza (CaCO3)  = %.0f mg/L\n", water_hardness);
 
-        //TelegramSendMessage(BOT_TOKEN, CHAT_ID, message, (const char *)_binary_telegram_cert_pem_start);
         TelegramSendMessage(BOT_TOKEN, CHAT_ID, buffer, (const char *)_binary_telegram_cert_pem_start);
         UartSendString(UART_PC, "Mensaje enviado al bot de Telegram\n");
     }
@@ -144,11 +111,22 @@ static void measuringResistanceFunction(void *pvParameter){
         // Convertir a voltaje
         v_adc = ((float)adc_raw/1000);
 
-        // Calcular resistencia del agua si está en un rango válido
+        // Calcular si se está en un voltaje válido
         if (v_adc < 3.3) {
-            r_water = ((v_adc * R1) / (3.3 - v_adc))*2.83; //2,83 es el valor de calibración.
-            conductivity = (2.27 / r_water) * 1000000; // Convertir a µS/cm. Constante de celda: 2,27 1/cm
+            r_water = ((v_adc * R1) / (3.3 - v_adc))*2.83; //2,83 es el valor de calibración. Se obtuvo experimentalmente.
+            conductivity = (2.27 / r_water) * 1000000; // Convertir a µS/cm. Constante de celda: 2,27 [1/cm]
             water_hardness = CONVERSION_FACTOR*conductivity;
+
+            uint8_t new_led_state = (water_hardness > UMBRAL) ? 1 : 0;
+
+            if (new_led_state != led_state) {
+                if (new_led_state) {
+                    LedOn(LED_1);
+                } else {
+                    LedOff(LED_1);
+                }
+                led_state = new_led_state;
+            }
         }
         UartSendString(UART_PC, buffer);
     }
@@ -156,10 +134,7 @@ static void measuringResistanceFunction(void *pvParameter){
 
 /*==================[external functions definition]==========================*/
 void app_main(void) {
-    
-    printf("P.Electronics System\n");
-
-    //Parte del main que corresponde a la medición de la resistencia    
+      
     serial_config_t my_uart = {
 		.port = UART_PC,
 		.baud_rate = 115200,
@@ -193,10 +168,10 @@ void app_main(void) {
     };
     TimerInit(&timer_for_telegram);
 
-    ServoInit(SERVO_0,GPIO_4);
-
     WifiInit(WIFI_SSID, WIFI_PASS);
     vTaskDelay(pdMS_TO_TICKS(3000)); // Esperar conexión
+
+    LedsInit();
 
     PWMInit(PWM_0, GPIO_9, 1000);     
     PWMSetDutyCycle(PWM_0, 50);
